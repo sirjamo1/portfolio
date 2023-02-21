c692ac97309e2903c17833036d66aad8091a8006
Sirjamo1 <jam_how@hotmail.com>
2023-02-21 13:34:10 +1000

    linked github link in footer

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,15 +2,15 @@ import SocialLinks from "./SocialLinks";
 
 const Contact = () => {
     return (
-        <div className="contact-container">
+        <footer className="contact-container">
             <h2>Contact me</h2>
             <h4>Please get in touch</h4>
             <p>Located in Brisbane, Australia</p>
             <p>Mob: 0448 121 814</p>
             <p>E-mail : jam_how@hotmail.com</p>
 
-            <SocialLinks />
-        </div>
+            <SocialLinks gitHubHref={"https://github.com/sirjamo1"} />
+        </footer>
     );
 };
 
