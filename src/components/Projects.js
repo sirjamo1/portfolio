2b9f6c44a865e6745ac6a27606b73ce3aa5a97e3
Sirjamo1 <jam_how@hotmail.com>
2023-02-23 13:50:15 +1000

    added show more projects button (only appears when screen < 700

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -25,12 +25,14 @@ import { ReactComponent as JavaScriptSvg } from "../assets/skills/javaScript.svg
 import { ReactComponent as ReactJsSvg } from "../assets/skills/reactJs.svg";
 import { ReactComponent as FirebaseSvg } from "../assets/skills/firebase.svg";
 import { ReactComponent as WebpackSvg } from "../assets/skills/webpack.svg";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { AiFillGithub } from "react-icons/ai";
 import { MdOutlineMobileFriendly, MdComputer } from "react-icons/md";
 import { RxOpenInNewWindow } from "react-icons/rx";
 
 const Projects = () => {
+    const [projectsRendered, setProjectsRendered] = useState(4);
+    const [projectsRenderedButton, setProjectsRenderedButton] = useState(true);
     const [projects, setProjects] = useState([
         {
             name: "Wheres Waldo",
@@ -300,132 +302,181 @@ const Projects = () => {
         }
         setProjects(prevProjects);
     };
-
+    const changeProjectsRendered = () => {
+        projectsRendered > 4
+            ? setProjectsRendered(4)
+            : setProjectsRendered(projects.length);
+    };
+    const removeShowMoreButton = () => {
+        if (window.innerWidth > 700) {
+            setProjectsRenderedButton(false);
+            setProjectsRendered(projects.length);
+        } else {
+            setProjectsRenderedButton(true);
+        }
+    };
+    useEffect(() => {
+        window.addEventListener("resize", removeShowMoreButton);
+    });
     return (
         <div className="projects-container">
-            {projects.map((project) => {
-                return (
-                    <div key={project.key} className="project-card">
-                        <img
-                            className="project-card-screenshot"
-                            src={project.src500}
-                            srcSet={`${project.src500} 500w, 
+            {projects.map((project, j) => {
+                if (j < projectsRendered)
+                    return (
+                        <div key={project.key} className="project-card">
+                            <img
+                                className="project-card-screenshot"
+                                src={project.src500}
+                                srcSet={`${project.src500} 500w, 
                     ${project.src1000} 1000w`}
-                            alt={`${project.name} screenshot`}
-                        />
-                        <div className="project-card-title-icons-repo-links-container">
-                            <div className="project-card-title-icons-container">
-                                <h3>{project.name}</h3>
-                                <div className="compatible-made-with-container">
-                                    <div className="compatible-icons">
-                                        {project.compatible.map((icon) => {
-                                            return icon;
-                                        })}
-                                    </div>
-                                    <div className="made-with-icons">
-                                        {project.madeWith.map((icon) => {
-                                            return icon;
-                                        })}
+                                alt={`${project.name} screenshot`}
+                            />
+                            <div className="project-card-title-icons-repo-links-container">
+                                <div className="project-card-title-icons-container">
+                                    <h3>{project.name}</h3>
+                                    <div className="compatible-made-with-container">
+                                        <div className="compatible-icons">
+                                            {project.compatible.map((icon) => {
+                                                return icon;
+                                            })}
+                                        </div>
+                                        <div className="made-with-icons">
+                                            {project.madeWith.map((icon) => {
+                                                return icon;
+                                            })}
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div className="project-card-repo-links">
-                                <a
-                                    href={project.gitHubRepository}
-                                    target="_blank"
-                                    rel="noreferrer"
-                                    alt={`${project.name} github repository`}
-                                >
-                                    <AiFillGithub />
-                                </a>
+                                <div className="project-card-repo-links">
+                                    <a
+                                        href={project.gitHubRepository}
+                                        target="_blank"
+                                        rel="noreferrer"
+                                        alt={`${project.name} github repository`}
+                                    >
+                                        <AiFillGithub />
+                                    </a>
 
-                                <a
-                                    href={project.livePreview}
-                                    target="_blank"
-                                    rel="noreferrer"
-                                    alt={`${project.name} live preview`}
+                                    <a
+                                        href={project.livePreview}
+                                        target="_blank"
+                                        rel="noreferrer"
+                                        alt={`${project.name} live preview`}
+                                    >
+                                        <RxOpenInNewWindow />
+                                    </a>
+                                </div>
+                            </div>
+
+                            <div className="drop-down-container">
+                                <section
+                                    className="project-card-description"
+                                    style={{
+                                        opacity: project.showDescription
+                                            ? "1"
+                                            : "0",
+                                        fontSize: project.showDescription
+                                            ? "14px"
+                                            : "0px",
+                                        zIndex: project.showDescription
+                                            ? "99"
+                                            : "-99",
+                                    }}
                                 >
-                                    <RxOpenInNewWindow />
-                                </a>
+                                    <h5
+                                        style={{
+                                            fontSize: project.showDescription
+                                                ? "22px"
+                                                : "0px",
+                                        }}
+                                    >
+                                        Description
+                                    </h5>
+                                    <p>{project.description}</p>
+                                </section>
+                                <section
+                                    className="project-card-features"
+                                    style={{
+                                        opacity: project.showFeatures
+                                            ? "1"
+                                            : "0",
+                                        fontSize: project.showFeatures
+                                            ? "14px"
+                                            : "0px",
+                                        zIndex: project.showFeatures
+                                            ? "99"
+                                            : "-99",
+                                        padding: project.showFeatures
+                                            ? "3px 10px"
+                                            : "0px 0px",
+                                    }}
+                                >
+                                    <h5
+                                        style={{
+                                            fontSize: project.showFeatures
+                                                ? "22px"
+                                                : "0px",
+                                        }}
+                                    >
+                                        Features
+                                    </h5>
+                                    <ul>
+                                        {project.features.map((feature, i) => {
+                                            return (
+                                                <li key={`${project.key}${i}`}>
+                                                    {feature}
+                                                </li>
+                                            );
+                                        })}
+                                    </ul>
+                                </section>
+
+                                <div className="show-hide-button-container">
+                                    <button
+                                        className="show-hide-description-button"
+                                        style={{
+                                            animation: project.showDescription
+                                                ? "activeButtonGradient 0.3s linear both"
+                                                : "activeButtonGradientReverse 0.3s linear both",
+                                        }}
+                                        onClick={() =>
+                                            showHideDescription(project.key)
+                                        }
+                                    >
+                                        {project.showDescription
+                                            ? "Hide description"
+                                            : "Show description"}
+                                    </button>
+                                    <button
+                                        className="show-hide-features-button"
+                                        style={{
+                                            animation: project.showFeatures
+                                                ? "activeButtonGradient 0.3s linear both"
+                                                : "activeButtonGradientReverse 0.3s linear both",
+                                        }}
+                                        onClick={() =>
+                                            showHideFeatures(project.key)
+                                        }
+                                    >
+                                        {project.showFeatures
+                                            ? "Hide features"
+                                            : "Show features"}
+                                    </button>
+                                </div>
                             </div>
                         </div>
-
-                        <div className="drop-down-container">
-                            <section
-                                className="project-card-description"
-                                style={{
-                                    opacity: project.showDescription
-                                        ? "1"
-                                        : "0",
-                                    fontSize: project.showDescription
-                                        ? "12px"
-                                        : "0px",
-                                    zIndex: project.showDescription
-                                        ? "99"
-                                        : "-99",
-                                }}
-                            >
-                                <h5>Description</h5>
-                                <p>{project.description}</p>
-                            </section>
-                            <section
-                                className="project-card-features"
-                                style={{
-                                    opacity: project.showFeatures ? "1" : "0",
-                                    fontSize: project.showFeatures
-                                        ? "12px"
-                                        : "0px",
-                                    zIndex: project.showFeatures ? "99" : "-99",
-                                }}
-                            >
-                                <h5>Features</h5>
-                                <ul>
-                                    {project.features.map((feature, i) => {
-                                        return (
-                                            <li key={`${project.key}${i}`}>
-                                                {feature}
-                                            </li>
-                                        );
-                                    })}
-                                </ul>
-                            </section>
-
-                            <div className="show-hide-button-container">
-                                <button
-                                    className="show-hide-description-button"
-                                    style={{
-                                        animation: project.showDescription
-                                            ? "activeButtonGradient 0.3s linear both"
-                                            : "activeButtonGradientReverse 0.3s linear both",
-                                    }}
-                                    onClick={() =>
-                                        showHideDescription(project.key)
-                                    }
-                                >
-                                    {project.showDescription
-                                        ? "Hide description"
-                                        : "Show description"}
-                                </button>
-                                <button
-                                    className="show-hide-features-button"
-                                    style={{
-                                        animation: project.showFeatures
-                                            ? "activeButtonGradient 0.3s linear both"
-                                            : "activeButtonGradientReverse 0.3s linear both",
-                                    }}
-                                    onClick={() =>
-                                        showHideFeatures(project.key)
-                                    }
-                                >
-                                    {project.showFeatures
-                                        ? "Hide features"
-                                        : "Show features"}
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                );
+                    );
             })}
+            {projectsRenderedButton ? (
+                <button
+                    className="load-more-projects"
+                    onClick={() => changeProjectsRendered()}
+                >
+                    Load {projectsRendered > 4 ? "less" : "more"} Projects
+                </button>
+            ) : (
+                <></>
+            )}
         </div>
     );
 };
